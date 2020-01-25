0b3439b6f8e322751a6360529b7874a37ad0091d
hitruk andrey <xyz@hitruk.com>
2020-01-25 17:34:34 +0300

    updated file test_Yandex.py

--- tests/test_Yandex.py
+++ tests/test_Yandex.py
@@ -1,15 +1,20 @@
 
 from selenium.webdriver.common.keys import Keys
-
+from page.search_Yandex import SearchYandexPage
 def test_search_yandex(browser):
 
-    URL = 'https://yandex.ru/'
     PHRASE = 'Hitruk'
+    # URL = 'https://yandex.ru/'
 
-    browser.get(URL)
+    # browser.get(URL)
+    #
+    # input_phrase = browser.find_element_by_id("text")
+    # input_phrase.send_keys(PHRASE+Keys.RETURN)
 
-    input_phrase = browser.find_element_by_id("text")
-    input_phrase.send_keys(PHRASE+Keys.RETURN)
+    search_page = SearchYandexPage(browser)
+    search_page.load_page()
+    search_page.input_phrase(PHRASE)
+
 
     result_blocks = browser.find_elements_by_css_selector('li.serp-item')
     assert len(result_blocks) > 0
